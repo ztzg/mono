c5bcc0a372ce86aeb01ed8a1a1e85edccc2e5d92
Adar Wesley <adar@mono-cvs.ximian.com>
2007-05-07 09:48:47 +0000

    merge -r 76814:76818
    svn path=/branches/mainsoft/gh20/mcs/; revision=76820

--- mcs/class/System.Web.Services/System.Web.Services.Description/ProtocolReflector.cs
+++ mcs/class/System.Web.Services/System.Web.Services.Description/ProtocolReflector.cs
@@ -461,6 +461,14 @@ namespace System.Web.Services.Description {
 			return null;
 		}
 
+#if NET_2_0
+		[MonoNotSupported("Not Implemented")]
+		protected virtual void ReflectDescription () 
+		{
+			throw new NotImplementedException ();
+		}
+#endif
+
 		#endregion
 	}
 }
