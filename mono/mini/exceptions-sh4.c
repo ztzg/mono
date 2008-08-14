c842145e9e66db8616df55085b7b30cda8364d8b
Cedric VINCENT <cedric.vincent@st.com>
2008-08-14 15:46:43 +0200

    Add initial implementation of the call to exception filters.

--- mono/mini/exceptions-sh4.c
+++ mono/mini/exceptions-sh4.c
@@ -20,11 +20,111 @@ MonoJitInfo *mono_arch_find_jit_info(MonoDomain *domain, MonoJitTlsData *jit_tls
 	return NULL;
 }
 
+/**
+ * Returns a pointer to a method which calls an exception filter.
+ *
+ * Technically, the generated method is similar to this pseudo-code :
+ *
+ * void call_filter(MonoContext *ctx, rtype (* handler)(void))
+ * {
+ * 	// Save all callee-saved registers (a.k.a global registers), except the stack pointer.
+ * 	regs[] = { %R8, ... , %R14 };
+ * 
+ * 	unsigned int regs[8];
+ * 
+ *  	// Restore all global registers from the MonoContext, except the stack pointer.
+ * 	{ %R8, ... , %R14 } = ctx.regs[8..14];
+ * 
+ * 	// Call the handler.
+ * 	handler();
+ * 
+ *  	// Restore all callee-saved registers from the stack, except the stack pointer.
+ * 	{ %R8, ... , %R14 } = regs[];
+ * 
+ * 	return;
+ * }
+ */
 gpointer mono_arch_get_call_filter(void)
 {
-	/* TODO - CV */
-	g_assert(0);
-	return NULL;
+	static guint8 *code = NULL;
+	guint8 *buf = NULL;
+	int i = 0;
+
+#define CALL_FILTER_SIZE 58
+
+	if (code != NULL)
+		return code;
+
+	code = buf = mono_global_codeman_reserve(CALL_FILTER_SIZE);
+
+	/* Save the return address. */
+	sh4_stsl_PR_decRx(buf, sh4_r15);
+
+	/* pseudo-code: unsigned int regs[8]; */
+	sh4_add_imm(buf, - (7 * 4), sh4_r15);
+
+	/* At this point, the stack looks like :
+	 *	:          :
+	 *	|          | Caller's frame.
+	 *	|==========|
+	 *	|    PR    |
+	 *	|----------|
+	 *	|          | Current frame.
+	 *	|  regs[]  |
+	 *	|          |
+	 *	|----------| <- SP
+	 *	:          :
+	 */
+
+	/*
+	 * Save all callee-saved registers (a.k.a global registers), except the stack pointer.
+	 */
+
+	/* pseudo-code: regs[] = { %R8, %R9, %R10, %R11, %R12, %R13, %R14 }; */
+	for (i = 8; i <= 14; i++)
+		sh4_movl_dispRx(buf, (SH4IntRegister)i, (i - 8) * 4, sh4_r15);
+
+	/*
+	 * Restore all global registers from the MonoContext, except the stack pointer.
+	 */
+
+	/* R4 is now used to point to "ctx.regs[]". */
+	sh4_add_imm(buf, offsetof(MonoContext, regs), sh4_r4);
+
+	/* pseudo-code: { %R8, ... , %R14 } = ctx.regs[8..14]; */
+	for (i = 8; i <= 13; i++) /* R14 is set into the next delay slot. */
+		sh4_movl_dispRy(buf, i * 4, sh4_r4, (SH4IntRegister)i);
+
+	/*
+	 * Call the handler.
+	 */
+
+	/* pseudo-code: handler(); */
+	sh4_jsr_indRx(buf, sh4_r5);
+	sh4_movl_dispRy(buf, 14 * 4, sh4_r4, sh4_r14);
+	/* ^ Optim ^ : "%R14 = ctx.regs[14];" into the delay slot. */
+
+	/*
+	 * Restore all callee-saved registers from the stack.
+	 */
+
+	/* pseudo-code: { %R8, ... , %R14 } = regs[]; */
+	for (i = 8; i <= 14; i++)
+		sh4_movl_dispRy(buf, (i - 8) * 4, sh4_r15, (SH4IntRegister)i);
+
+	/* pseudo-code: return; */
+	sh4_add_imm(buf, (7 * 4), sh4_r15);
+	sh4_ldsl_incRx_PR(buf, sh4_r15);
+	sh4_rts(buf);
+	sh4_nop(buf);
+
+	/* Sanity checks. */
+	g_assert(buf - code <= CALL_FILTER_SIZE);
+
+	/* Flush instruction cache, since we've generated code. */
+	mono_arch_flush_icache(code, CALL_FILTER_SIZE);
+
+	return code;
 }
 
 /**
